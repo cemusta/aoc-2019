3f5fbf9f02e05297129beb91fdfd1d03d5c2c4b6
Cem Usta <nooneelse@gmail.com>
2020-06-15 16:27:09 +0200

    day 2 part 2 solved

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ const { run } = require('./modules/intCodeProcessor')
 
 const fs = require('fs')
 
-logger.info('task starting')
-
 const launchCalculation = () => {
   const starshipParts = fs.readFileSync('./inputs/day1.input', 'utf8').split('\n').map(x => Number(x))
   const fuelNeeded = starshipParts.reduce((accumulator, currentValue) => accumulator + calculateRecursiveFuel(currentValue), 0)
@@ -16,11 +14,45 @@ const launchCalculation = () => {
 const runIntCode = () => {
   const codeArray = fs.readFileSync('./inputs/day2.input', 'utf8').split(',').map(x => Number(x))
   // manual override - 1202
-  codeArray[1] = 12
-  codeArray[2] = 2
+  codeArray[1] = 82
+  codeArray[2] = 62
   const result = run(codeArray)
   logger.info(`Day 2: opCode result is ${result[0]}`)
+  return result[0]
 }
 
-launchCalculation()
-runIntCode()
+const searchForOutput = (searchedResult = 19690720) => {
+  logger.info(`searching verb and noun for ${searchedResult}`)
+  // search for spesific IntCode output
+  for (let noun = 0, verb = 0; verb < 100; verb++) {
+    const codeArray = fs.readFileSync('./inputs/day2.input', 'utf8').split(',').map(x => Number(x))
+
+    codeArray[1] = verb
+    codeArray[2] = noun
+    const result = run(codeArray)
+
+    if (Number(result[0]) === searchedResult) {
+      logger.info(`${verb}:${noun} result ${result[0]} a match, result key: ${verb}${noun}`)
+      return
+    } else if (result[0] > searchedResult) {
+      logger.info(`${verb}:${noun} result ${result[0]} too big`)
+      if (noun < 100) {
+        noun++
+        verb = 0
+      } else {
+        break
+      }
+    }
+    logger.info(`${verb}:${noun} result ${result[0]} too small`)
+  }
+}
+
+logger.info('task starting')
+// call needed operation here
+searchForOutput()
+
+module.exports = {
+  launchCalculation,
+  runIntCode,
+  searchForOutput
+}
