40b90d20996cd110babcfe385d75719a5380c6f3
Cem Usta <nooneelse@gmail.com>
2020-06-19 13:30:08 +0200

    refactoring index.js and adding tests.

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
+const { calculateRecursiveFuel } = require('../src/modules/fuelCalculator')
+const { run } = require('../src/modules/intCodeProcessor')
 const { countOrbitCheckSums, minimumTransferNeeded } = require('../src/modules/orbitCalculator')
+const { nearestIntersection } = require('../src/modules/wireOperator')
+const { bruteForce } = require('../src/modules/passwordOperations')
 
 describe('general solution', () => {
   test('should work without a problem', () => {
@@ -7,16 +11,71 @@ describe('general solution', () => {
   })
 })
 
+describe('day 1 part 2 solution', () => {
+  test('should solve problem', () => {
+    const starshipParts = fs.readFileSync('./inputs/day1.input', 'utf8').split('\n').map(x => Number(x))
+    const fuelNeeded = starshipParts.reduce((accumulator, currentValue) => accumulator + calculateRecursiveFuel(currentValue), 0)
+    // logger.info(`Day 1: Needed fuel for launch is ${fuelNeeded}`)
+    expect(fuelNeeded).toBe(4995942)
+  })
+})
+
 describe('day 2 part 1 solution', () => {
   test('should solve problem', () => {
-    const result = require('../src/index.js').runIntCode()
-    expect(result).toBe(3101878)
+    const codeArray = fs.readFileSync('./inputs/day2.input', 'utf8').split(',').map(x => Number(x))
+    // manual override - 1202
+    codeArray[1] = 12
+    codeArray[2] = 2
+    const result = run(codeArray.join(','))
+    // logger.info(`Day 2: opCode result is ${result[0]}`)
+    expect(result[0]).toBe(3101878)
+  })
+})
+
+describe('day 2 part 2 solution', () => {
+  test('should solve problem', () => {
+    const searchedResult = 19690720
+    // search for spesific IntCode output
+    for (let noun = 0, verb = 0; verb < 100; verb++) {
+      const codeArray = fs.readFileSync('./inputs/day2.input', 'utf8').split(',').map(x => Number(x))
+
+      codeArray[1] = verb
+      codeArray[2] = noun
+      const result = run(codeArray.join(','))
+
+      if (Number(result[0]) === searchedResult) {
+        // logger.info(`${verb}:${noun} result ${result[0]} a match, result key: ${verb}${noun}`)
+        expect(verb).toBe(84)
+        expect(noun).toBe(44)
+        return
+      } else if (result[0] > searchedResult) {
+        // logger.info(`${verb}:${noun} result ${result[0]} too big`)
+        if (noun < 100) {
+          noun++
+          verb = 0
+        } else {
+          break
+        }
+      }
+      // logger.info(`${verb}:${noun} result ${result[0]} too small`)
+    }
+    // logger.info(`Day 2: opCode result is ${result[0]}`)
+  })
+})
+
+describe('day 3 part 1-2 solution', () => {
+  test('should solve problem', () => {
+    const wires = fs.readFileSync('./inputs/day3.input', 'utf8').split('\n')
+    const result = nearestIntersection(wires[0], wires[1])
+    // logger.info(`Day 3: closest wire intersection is ${result[0]}, shortest steps is ${result[1]}`)
+    expect(result[0]).toBe(1225)
+    expect(result[1]).toBe(107036)
   })
 })
 
 describe('day 4 part 2 solution', () => {
   test('should solve problem', () => {
-    const result = require('../src/index.js').bruteForce(347312, 805915)
+    const result = bruteForce(347312, 805915)
     expect(result).toBe(364)
   })
 })
